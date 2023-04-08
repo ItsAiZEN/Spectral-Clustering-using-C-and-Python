2f118cb670c92bc53d0747772005090505e0fac9
Tal Sabel <talsabel@nova.cs.tau.ac.il>
2023-04-08 08:43:50 +0300

    module build works (with some warnings) and no import error when running python. Now there is segmentation fault - need to debug module

--- Python/setup.py
+++ Python/setup.py
@@ -1,6 +1,6 @@
 from setuptools import Extension, setup
 
-module = Extension("mykmeanssp", sources=['spkmeansmodule.c'])
+module = Extension("mykmeanssp", sources=['spkmeansmodule.c','spkmeans.c'])
 setup(name='mykmeanssp',
       version='1.0',
       description='A module that performs spectral clustering and returns wam, ddg, gl, jacobi matrices',
