a69c427338b55a06d107b2bb896d7c0e3e59efcb
Nir Eizenman <64685062+ItsAiZEN@users.noreply.github.com>
2023-04-09 15:57:11 +0300

    Delete C directory

--- C/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import Extension, setup
-
-module = Extension("mykmeanssp", sources=['spkmeansmodule.c'])
-setup(name='mykmeanssp',
-      version='1.0',
-      description='A module that performs spectral clustering and returns wam, ddg, gl, jacobi matrices',
-      ext_modules=[module])
